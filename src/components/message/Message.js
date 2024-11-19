987d9caf6631c56603a21ef3704ee7d38583f77e
opercy <opercy@paypal.com>
2024-11-19 10:00:07 -0600

    update comment

--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -187,7 +187,7 @@ const Message = function ({ markup, meta, parentStyles, warnings }) {
                 request('GET', `${window.location.origin}/credit-presentment/smart/message?${query}`).then(
                     ({ data: resData }) => {
                         const jsonData = resData.slice(resData.indexOf('<!--') + 4, resData.indexOf('-->'));
-                        // TODO: Cleanup ternary as 'parseObjFromEncoding' will no longer be used; only JSON.parse will be required.
+                        // TODO: Cleanup ternary and remove 'parseObjFromEncoding' from utils; only JSON.parse will be needed.
                         const data = jsonData.startsWith('{') ? JSON.parse(jsonData) : parseObjFromEncoding(jsonData);
                         button.innerHTML = data.markup ?? markup ?? '';
                         const buttonWidth = button.offsetWidth;
